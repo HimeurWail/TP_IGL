31fc908631047fa648c3fbdb49832e8bf67c522e
Razane Meguellati <kl_meguellati@esi.dz>
2023-01-03 22:14:56 +0100

    for wail

--- react_app/src/Components/Navbar.js
+++ react_app/src/Components/Navbar.js
@@ -1,7 +1,34 @@
 import { Link } from 'react-router-dom' ; 
-
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import { useState , useEffect } from 'react';
 
 const Navbar = () => {
+    const [ profile, setProfile ] = useState([]);
+    const [authentificated, setAuthentificated] = useState(false)
+    const clientId = '763504218710-jee3n3jn6hvd315dfukpfet4hbjf869m.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            })
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+        setAuthentificated(true);
+    };
+
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        setAuthentificated(false);
+    };
     return ( 
         <nav className="navbar sticky top-0 p-[5px] m-auto  bg-azra9 cursor-pointer font-normal top-0 flex flex-row items-center jutify-center px-[15px] py-[10px] w-full">
           <div className=''> 
@@ -20,7 +47,16 @@ const Navbar = () => {
           </div>
 
           <div className="sign px-[50px] items-end flex ">
-            <Link className="bg-ahmar m-2 p-2 hover:text-white rounded-[12px]"  to="/SignIn"> Sign up  </Link>
+            <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    prompt="select_account"
+                    isSignedIn={true}
+                    className='bg-ahmar m-2 p-2 hover:text-white rounded-[12px]'
+            />
          </div>
            
       
