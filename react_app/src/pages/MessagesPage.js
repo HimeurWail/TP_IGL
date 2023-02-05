426527ae8cabda25ccf35b7c4e684f65f98d58b4
Razane Meguellati <kl_meguellati@esi.dz>
2023-02-06 00:06:43 +0100

    save changes

--- react_app/src/pages/MessagesPage.js
+++ react_app/src/pages/MessagesPage.js
@@ -20,8 +20,8 @@ const MessagesPage = (props) => {
 
     return (
         <div className="bg-beige pt-[3%] flex flex-col space-y-4">
-            <div className="flex flex-row justify-center space-x-5">
-                    <img src={messageicon} className=" lg:w-[50px]" alt="" />
+            <div className="flex flex-row justify-center  space-x-2 lg:space-x-5">
+                    <img src={messageicon} className=" w-[30px] lg:w-[50px]" alt="" />
                     <Title first={' Your Messages '}></Title>
             </div>
 
@@ -31,18 +31,30 @@ const MessagesPage = (props) => {
         
                 (
                    <div className="flex flex-col m-6">
-                    <div className=" flex flex-col  mx-auto mb-[1px] border-lightgris border-2 rounded-xl w-[60%] p-6">
-                   <div className="flex flex-row space-x-4">
-                   <img src={checked} className=" lg:w-[25px]" alt="" />
-                   <p className="text-lg font-bold "> Message from : {Msg.senderEmail} </p>
-                    </div>    
+                    <div className=" flex flex-col  mx-auto mb-[1px] border-lightgris border-2 rounded-xl w-[90%] lg:w-[60%] lg:p-6 p-4">
+                   <div className="flex flex-row  space-x-1 text-center  lg:space-x-4">
+                   <img src={checked} className="w-[30px] h-[30px] lg:w-[25px] " alt="" />
+
+                 <div className="flex flex-col lg:flex-row space-x-3 ">
+                   <p className="lg:text-lg text-sm font-bold "> Message from  :    </p>
+                   <p className="lg:text-lg text-sm  ">  {Msg.senderEmail}</p>
+                 </div>
                   
-                   <p className="text-md  font-semibold text-gris  text-sm "> Announce title : {Msg.title} </p>
-                   <p className="text-md  font-medium  text-sm "> sent on : {Msg.createdAt} </p>
+               
+                    </div>    
+                  <div className="flex flex-col text-center   lg:text-left" >
+                     
+                      <p className="lg:text-md  font-semibold text-gris  text-sm "> Announce title :{Msg.title} </p>
+                      <p className="lg:text-md  font-medium  text-sm "> sent on : {Msg.createdAt} </p>
+                  
+                   
                    <div className="bg-lightgris w-[100%] h-1 my-2"></div>
+                  
                  <p>
                     { Msg.message }
                  </p>
+                   </div>
+                   
                 
                 
                 </div>
@@ -52,13 +64,16 @@ const MessagesPage = (props) => {
                  <a href={"mailto: "+Msg.senderEmail} target="_blank" > Contact sender </a>
                  </button >
                  </div>
+                
 
                    </div> 
+                  
                 
                 ))
                }
 
             </div>
+            
         </div>
       );
 }
